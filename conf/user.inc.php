a06613c41e882613d7d0d8a70e4aa7c37cbe985f
hawai <hawai@projekt.de>
2015-04-21 13:25:14 +0200

    src in git-repo konvertiert

---
+++ conf/user.inc.php
@@ -0,0 +1,7 @@
+<?php
+$this->WFdbhost = "141.22.29.94";
+$this->WFdbname = "wawision";
+$this->WFdbuser = "mars";
+$this->WFdbpass = "wihaw2015AI";
+$this->WFuserdata = "/var/www/html/wawision_oss_testing/userdata";
+?>
